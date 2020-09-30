c313c7949ee13deb61e4c68ad3cf698866224cc2
Scott Prigmore <prig6030@stthomas.edu>
2020-09-30 18:34:42 -0500

    first failing test

---
+++ ISBNValidator/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -0,0 +1,14 @@
+package com.virtualpairprogrammers.isbntools;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ValidateISBNTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
