75954c6eb35f293cdcc9f6dc677670c52a00c247
Scott Prigmore <prig6030@stthomas.edu>
2020-09-30 19:54:17 -0500

    Green - First passing checkISBN by returning true

--- ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -3,8 +3,7 @@ package com.virtualpairprogrammers.isbntools;
 public class ValidateISBN {
 
 	public boolean checkISBN(int i) {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 }
