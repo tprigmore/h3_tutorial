68d89bf1a02d9a136f11179142e12321ecbc70de
Scott Prigmore <prig6030@stthomas.edu>
2020-10-04 17:53:36 -0500

    Green - added length check

--- ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -5,6 +5,8 @@ public class ValidateISBN {
 	public boolean checkISBN(String isbn) {
 		int total = 0;
 		
+		if(isbn.length() != 10) throw new RuntimeException("ISBN number must be 10 digits");
+		
 		for (int i = 0; i < 10; i++) {
 			total += isbn.charAt(i) * (10 - i);
 		}
