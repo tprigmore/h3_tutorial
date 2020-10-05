2a5b8e209a269c7e0b1bca5762a80f079ba76e30
Scott Prigmore <prig6030@stthomas.edu>
2020-10-04 20:55:25 -0500

    Green - added 13 digit checks

--- ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -4,27 +4,43 @@ public class ValidateISBN {
 
 	public boolean checkISBN(String isbn) {
 		int total = 0;
-		
-		if(isbn.length() != 10) throw new RuntimeException("ISBN number must be 10 digits");
-		
-		for (int i = 0; i < 10; i++) {
-			if ((i == 9) && (isbn.charAt(i) == 'X')) {
-				total += 10;
+		if(isbn.length() == 13) {
+			for (int i = 0; i < 13; i++) {
+				if (i % 2 == 0) {
+					total += Character.getNumericValue(isbn.charAt(i));
+				}
+				else {
+					total += Character.getNumericValue(isbn.charAt(i)) * 3;
+				}
 			}
-			else if (!Character.isDigit(isbn.charAt(i))) {
-				throw new RuntimeException("ISBN number can't have letters");
+			if(total % 10 == 0) {
+				return true;
 			}
 			else {
-			total += Character.getNumericValue(isbn.charAt(i)) * (10 - i);
+				return false;	
 			}
 		}
-
-		if(total % 11 == 0) {
-			return true;
-			}
 		else {
-			return false;	
+			if(isbn.length() != 10) throw new RuntimeException("ISBN number must be 10 digits");
+			
+			for (int i = 0; i < 10; i++) {
+				if ((i == 9) && (isbn.charAt(i) == 'X')) {
+					total += 10;
+				}
+				else if (!Character.isDigit(isbn.charAt(i))) {
+					throw new RuntimeException("ISBN number can't have letters");
+				}
+				else {
+				total += Character.getNumericValue(isbn.charAt(i)) * (10 - i);
+				}
 			}
 	
+			if(total % 11 == 0) {
+				return true;
+			}
+			else {
+				return false;	
+			}
+		}
 	}
 }
