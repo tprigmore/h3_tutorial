f5c6b7150435cd0e41755755f6974c231925c44c
Scott Prigmore <prig6030@stthomas.edu>
2020-10-04 21:15:48 -0500

    Blue - refactored code

--- ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -2,45 +2,52 @@ package com.virtualpairprogrammers.isbntools;
 
 public class ValidateISBN {
 
+	private static final int SHORT_ISBN_MULTIPLIER = 11;
+	private static final int LONG_ISBN_MULTIPLIER = 10;
+	private static final int SHORT_ISBN_LENGTH = 10;
+	private static final int LONG_ISBN_LENGTH = 13;
+
 	public boolean checkISBN(String isbn) {
-		int total = 0;
-		if(isbn.length() == 13) {
-			for (int i = 0; i < 13; i++) {
-				if (i % 2 == 0) {
-					total += Character.getNumericValue(isbn.charAt(i));
-				}
-				else {
-					total += Character.getNumericValue(isbn.charAt(i)) * 3;
-				}
-			}
-			if(total % 10 == 0) {
-				return true;
-			}
-			else {
-				return false;	
-			}
+		if(isbn.length() == LONG_ISBN_LENGTH) {
+			return isThisValidLongISBN(isbn);
+		}
+		else if(isbn.length() == SHORT_ISBN_LENGTH) {
+			return isThisAValidShortISBN(isbn);
 		}
 		else {
-			if(isbn.length() != 10) throw new RuntimeException("ISBN number must be 10 digits");
-			
-			for (int i = 0; i < 10; i++) {
-				if ((i == 9) && (isbn.charAt(i) == 'X')) {
-					total += 10;
-				}
-				else if (!Character.isDigit(isbn.charAt(i))) {
-					throw new RuntimeException("ISBN number can't have letters");
-				}
-				else {
-				total += Character.getNumericValue(isbn.charAt(i)) * (10 - i);
-				}
+			throw new RuntimeException("ISBN number must be 10 or 13 digits");
+		}
+	}
+
+	private boolean isThisAValidShortISBN(String isbn) {
+		int total = 0;
+
+		for (int i = 0; i < SHORT_ISBN_LENGTH; i++) {
+			if ((i == 9) && (isbn.charAt(i) == 'X')) {
+				total += 10;
 			}
-	
-			if(total % 11 == 0) {
-				return true;
+			else if (!Character.isDigit(isbn.charAt(i))) {
+				throw new RuntimeException("ISBN number can't have letters");
 			}
 			else {
-				return false;	
+			total += Character.getNumericValue(isbn.charAt(i)) * (SHORT_ISBN_LENGTH - i);
 			}
 		}
+
+		return (total % SHORT_ISBN_MULTIPLIER == 0) ;
+	}
+
+	private boolean isThisValidLongISBN(String isbn) {
+		int total = 0;
+
+		for (int i = 0; i < LONG_ISBN_LENGTH; i++) {
+			if (i % 2 == 0) {
+				total += Character.getNumericValue(isbn.charAt(i));
+			}
+			else {
+				total += Character.getNumericValue(isbn.charAt(i)) * 3;
+			}
+		}
+		return (total % LONG_ISBN_MULTIPLIER == 0) ;
 	}
 }
